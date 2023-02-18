27e3f972ad4aa3e162b9408489d8c538c1fe2642
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-18 21:06:21 +0600

    Update auth

---
+++ google_calendar_analytics/authentication/auth.py
@@ -0,0 +1,48 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+class CalendarAuth:
+    SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+    def __init__(self, token_path: str, credentials_path: str):
+        self.token_path = token_path
+        self.credentials_path = credentials_path
+
+    def get_credentials(self) -> Credentials | None:
+        """
+        Get the user's calendar credentials.
+
+        Returns:
+            Credentials: Google calendar credentials.
+        """
+        creds = None
+
+        if os.path.exists(self.token_path):
+            creds = Credentials.from_authorized_user_file(self.token_path, self.SCOPES)
+
+        if not creds or not creds.valid:
+            # Try to refresh the credentials
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                # If the credentials are not valid, try to get new credentials
+                try:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        self.credentials_path, self.SCOPES
+                    )
+                    creds = flow.run_local_server(port=0)
+                except FileNotFoundError:
+                    print(
+                        f"Error: The credentials file {self.credentials_path} was not found."
+                    )
+                    return None
+
+            # Save the credentials to the token file
+            with open(self.token_path, "w") as token:
+                token.write(creds.to_json())
+
+        return creds
---
+++ google_calendar_analytics/data_collecting/collector.py
@@ -0,0 +1,45 @@
+from datetime import timedelta, datetime
+
+from googleapiclient.discovery import build
+
+
+class CalendarDataCollector:
+    """A class to collect data from a Google Calendar."""
+
+    def __init__(self, creds):
+        self.service = build("calendar", "v3", credentials=creds)
+
+    def _get_events_by_time_range(
+            self,
+            time_min,
+            time_max,
+            calendar_id: str,
+    ) -> list:
+        """Helper function to retrieve events in a specific time range."""
+        events_result = (
+            self.service.events()
+            .list(
+                calendarId=calendar_id,
+                timeMin=time_min,
+                timeMax=time_max,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        return events_result.get("items", [])
+
+    def collect_data(
+            self,
+            start_time: datetime,
+            end_time: datetime,
+            calendar_id: str = 'primary'
+    ) -> list:
+        """Collect data from the calendar for the specified time range."""
+
+        events = self._get_events_by_time_range(
+            calendar_id=calendar_id,
+            time_min=start_time.isoformat() + "Z",
+            time_max=end_time.isoformat() + "Z",
+        )
+        return events
