0de415c466086389c4e78611daf4dc61bd0fb9fe
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-20 00:26:21 +0600

    Update init for pypi

---
+++ google_calendar_analytics/__init__.py
@@ -0,0 +1,15 @@
+"""
+:authors: Berupor
+:license: MIT
+
+:copyright: (c) 2023 Berupor
+"""
+
+from .collecting.collector import CalendarDataCollector
+from .processing.transformer import DataTransformer
+from .visualization.visualizer_factory import PlotFactory
+from .analytics import AnalyzerFacade
+
+__author__ = "Berupor"
+__version__ = "0.0.2"
+__email__ = "evgeniy.zelenoff@gmailc.com"
