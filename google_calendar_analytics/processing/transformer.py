1617acc5b9fe516e30892c5298447e1e6afdd8c1
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-03-12 00:18:43 +0600

    Add new processing for event

--- google_calendar_analytics/processing/transformer.py
+++ google_calendar_analytics/processing/transformer.py
@@ -15,7 +15,7 @@ class DataTransformer:
         return round(duration.total_seconds() / 3600, 2)
 
     def many_events_duration(
-        self, events: list[dict], max_events: int = 5, ascending=False
+            self, events: list[dict], max_events: int = 5, ascending=False
     ) -> pd.DataFrame:
         """
         A method to calculate the total duration of many events
@@ -70,3 +70,57 @@ class DataTransformer:
                 one_event[date] = one_event.get(date, 0) + duration
 
         return pd.DataFrame(one_event.items(), columns=["Date", "Duration"])
+
+    def event_duration_periods(
+            self,
+            events: list[dict],
+            event_name: str,
+            period_days: int,
+            num_periods: int,
+    ) -> pd.DataFrame:
+        """
+        A method to calculate the total duration of a single event for the current and previous periods.
+
+        Args:
+            events: A list of events represented as dictionaries with start and end times.
+            event_name: The name of the event to calculate the duration for.
+            period_days: The number of days in each period.
+            num_periods: The number of periods to calculate.
+
+        Returns:
+            Returns a pandas DataFrame with the event's total duration for each day in the format 'YYYY-MM-DD'.
+            Columns 'Date', 'Day', and 'Duration' display the date, the day number within each period, and the event's duration in hours, respectively.
+        """
+
+        period_duration = datetime.timedelta(days=period_days)
+        periods = []
+        for i in range(num_periods):
+            period_end = (
+                    datetime.datetime.now().date()
+                    - datetime.timedelta(days=datetime.datetime.now().weekday())
+                    - period_duration * i
+            )
+            period_start = period_end - period_duration + datetime.timedelta(days=1)
+            periods.append((period_start, period_end))
+
+        event_duration = {}  # type: ignore
+        for event in events:
+            if event["summary"] == event_name:
+                start, end = event["start"]["dateTime"], event["end"]["dateTime"]
+                duration = self._get_duration(start, end)
+                date = datetime.datetime.fromisoformat(start).date()
+
+                for period_index, (period_start, period_end) in enumerate(periods):
+                    if period_start <= date <= period_end:
+                        day_number = (date - period_start).days + 1
+                        date_str = date
+                        if date_str in event_duration:
+                            event_duration[date_str]["Duration"] += duration
+                        else:
+                            event_duration[date_str] = {
+                                "Date": date_str,
+                                "Day": day_number,
+                                "Duration": duration,
+                                "Period": period_index,
+                            }
+        return pd.DataFrame(list(event_duration.values()))
