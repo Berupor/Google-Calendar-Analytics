4674ed45610be787994f960343e6ebaddb211184
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-03-12 14:44:29 +0600

    Update sync processing to async

--- google_calendar_analytics/processing/transformer.py
+++ google_calendar_analytics/processing/transformer.py
@@ -3,8 +3,8 @@ import datetime
 import pandas as pd
 
 
-class DataTransformer:
-    def _get_duration(self, start, end) -> float:
+class AsyncDataTransformer:
+    async def _get_duration(self, start, end) -> float:
         """
         A method to calculate the duration of an event
         """
@@ -14,22 +14,20 @@ class DataTransformer:
         duration = end_time - start_time
         return round(duration.total_seconds() / 3600, 2)
 
-    def many_events_duration(
-            self, events: list[dict], max_events: int = 5, ascending=False
+    async def many_events_duration(
+        self, events: list[dict], max_events: int = 5, ascending=False
     ) -> pd.DataFrame:
         """
-        A method to calculate the total duration of many events
+        A method to calculate the duration of multiple events and return
+        a DataFrame with the top events
 
         Args:
-            events (list): A list of event objects with 'start' and 'end' times in ISO format and a 'summary' field.
-            max_events (int): The maximum number of events to include in the output dataframe.
-            ascending (bool): If True, sort the events in ascending order of duration.
-
-        Returns:
-            A pandas dataframe with columns 'Event' and 'Duration' for the top events by duration.
+            events (list[dict]): A list of events
+            max_events (int): The maximum number of events to be analyzed.
+            ascending (bool): If True, sort the events in ascending order of duration
         """
-
         event_durations: dict[str, float] = {}
+
         for event in events:
             # Get the start and end times in ISO format
             start, end = event.get("start", {}).get("dateTime"), event.get(
@@ -39,7 +37,7 @@ class DataTransformer:
             # If start and end times exist, calculate the duration and update the event durations dictionary
             if start and end:
                 summary = event["summary"]
-                duration = self._get_duration(start, end)
+                duration = await self._get_duration(start, end)
                 event_durations[summary] = event_durations.get(summary, 0) + duration
 
         events_series = pd.Series(event_durations)
@@ -48,57 +46,51 @@ class DataTransformer:
 
         return pd.DataFrame({"Event": top_events.index, "Duration": top_events.values})
 
-    def one_event_duration(self, events: list[dict], event_name: str) -> pd.DataFrame:
+    async def one_event_duration(
+        self, events: list[dict], event_name: str
+    ) -> pd.DataFrame:
         """
-        A method to calculate the total duration of a single event
+        A method to calculate the duration of one event and return
+        a DataFrame with the durations for each day
 
         Args:
-            events: A list of events represented as dictionaries with start and end times.
-            event_name: The name of the event to calculate the duration for.
-
-        Returns:
-            Returns a pandas DataFrame with the event's total duration for each date in the format 'MM.DD'.
-            Columns 'Date' and 'Duration' display the date and the event's duration in hours, respectively.
+            events (list[dict]): A list of events
+            event_name (str): The name of the event to be analyzed
         """
-
         one_event: dict[str, float] = {}
         for event in events:
             if event["summary"] == event_name:
                 start, end = event["start"]["dateTime"], event["end"]["dateTime"]
-                duration = self._get_duration(start, end)
+                duration = await self._get_duration(start, end)
                 date = datetime.datetime.fromisoformat(start).strftime("%m.%d")
                 one_event[date] = one_event.get(date, 0) + duration
 
         return pd.DataFrame(one_event.items(), columns=["Date", "Duration"])
 
-    def event_duration_periods(
-            self,
-            events: list[dict],
-            event_name: str,
-            period_days: int,
-            num_periods: int,
+    async def event_duration_periods(
+        self,
+        events: list[dict],
+        event_name: str,
+        period_days: int,
+        num_periods: int,
     ) -> pd.DataFrame:
         """
-        A method to calculate the total duration of a single event for the current and previous periods.
+        A method to calculate the duration of one event
+        in multiple periods and return a DataFrame with the durations for each day
 
         Args:
-            events: A list of events represented as dictionaries with start and end times.
-            event_name: The name of the event to calculate the duration for.
-            period_days: The number of days in each period.
-            num_periods: The number of periods to calculate.
-
-        Returns:
-            Returns a pandas DataFrame with the event's total duration for each day in the format 'YYYY-MM-DD'.
-            Columns 'Date', 'Day', and 'Duration' display the date, the day number within each period, and the event's duration in hours, respectively.
+            events (list[dict]): A list of events
+            event_name (str): The name of the event to be analyzed
+            period_days (int): The number of days in each period
+            num_periods (int): The number of periods to be analyzed
         """
-
         period_duration = datetime.timedelta(days=period_days)
         periods = []
         for i in range(num_periods):
             period_end = (
-                    datetime.datetime.now().date()
-                    - datetime.timedelta(days=datetime.datetime.now().weekday())
-                    - period_duration * i
+                datetime.datetime.now().date()
+                - datetime.timedelta(days=datetime.datetime.now().weekday())
+                - period_duration * i
             )
             period_start = period_end - period_duration + datetime.timedelta(days=1)
             periods.append((period_start, period_end))
@@ -107,7 +99,7 @@ class DataTransformer:
         for event in events:
             if event["summary"] == event_name:
                 start, end = event["start"]["dateTime"], event["end"]["dateTime"]
-                duration = self._get_duration(start, end)
+                duration = await self._get_duration(start, end)
                 date = datetime.datetime.fromisoformat(start).date()
 
                 for period_index, (period_start, period_end) in enumerate(periods):
@@ -123,4 +115,4 @@ class DataTransformer:
                                 "Duration": duration,
                                 "Period": period_index,
                             }
-        return pd.DataFrame(list(event_duration.values()))
+                            return pd.DataFrame(list(event_duration.values()))
