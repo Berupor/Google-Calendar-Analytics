d7bf7a4538193cf068bb163c8d1ded321665616d
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-03-18 18:23:12 +0600

    Update tests

--- tests/functional/src/data_processing/test_transformer.py
+++ tests/functional/src/data_processing/test_transformer.py
@@ -1,70 +1,105 @@
-import unittest
-
 import pandas as pd
+import pytest
 
-from google_calendar_analytics.processing.transformer import AsyncDataTransformer
+from google_calendar_analytics.processing.transformer import (
+    AsyncDataTransformer,
+    EventDurationPeriodsStrategy,
+    ManyEventsDurationStrategy,
+    OneEventDurationStrategy,
+)
 
 
-class DataTransformerTest(unittest.TestCase):
-    def setUp(self):
-        self.transformer = AsyncDataTransformer()
+@pytest.fixture()
+def many_events_duration_strategy():
+    return ManyEventsDurationStrategy()
 
-    async def test__get_duration(self):
-        start_time = "2022-02-18T09:00:00+00:00"
-        end_time = "2022-02-18T10:30:00+00:00"
-        expected_duration = 1.5
-        duration = self.transformer._get_duration(start_time, end_time)
 
-        self.assertAlmostEqual(duration, expected_duration, delta=0.01)
+@pytest.fixture()
+def one_event_duration_strategy():
+    return OneEventDurationStrategy()
 
-    async def test_many_events_duration(self):
-        events = [
-            {
-                "start": {"dateTime": "2022-02-18T09:00:00+00:00"},
-                "end": {"dateTime": "2022-02-18T10:30:00+00:00"},
-                "summary": "Meeting 1",
-            },
-            {
-                "start": {"dateTime": "2022-02-18T11:00:00+00:00"},
-                "end": {"dateTime": "2022-02-18T12:30:00+00:00"},
-                "summary": "Meeting 2",
-            },
-            {
-                "start": {"dateTime": "2022-02-18T13:00:00+00:00"},
-                "end": {"dateTime": "2022-02-18T14:30:00+00:00"},
-                "summary": "Meeting 3",
-            },
-        ]
-        expected_output = pd.DataFrame(
-            {
-                "Event": ["Meeting 1", "Meeting 2", "Meeting 3"],
-                "Duration": [1.5, 1.5, 1.5],
-            }
+
+@pytest.fixture()
+def event_duration_periods_strategy():
+    return EventDurationPeriodsStrategy()
+
+
+@pytest.fixture()
+def async_data_transformer():
+    return AsyncDataTransformer()
+
+
+@pytest.fixture()
+def sample_events():
+    return [
+        {
+            "summary": "Event 1",
+            "start": {"dateTime": "2022-03-17T09:00:00+00:00"},
+            "end": {"dateTime": "2022-03-17T11:00:00+00:00"},
+        },
+        {
+            "summary": "Event 2",
+            "start": {"dateTime": "2022-03-18T09:00:00+00:00"},
+            "end": {"dateTime": "2022-03-18T12:00:00+00:00"},
+        },
+        {
+            "summary": "Event 3",
+            "start": {"dateTime": "2022-03-19T09:00:00+00:00"},
+            "end": {"dateTime": "2022-03-19T13:00:00+00:00"},
+        },
+    ]
+
+
+@pytest.mark.asyncio
+async def test_many_events_duration_strategy_calculate_duration(
+        many_events_duration_strategy, sample_events
+):
+    result = await many_events_duration_strategy.calculate_duration(
+        sample_events, max_events=2
+    )
+    assert isinstance(result, pd.DataFrame)
+    assert len(result) == 2
+    assert all(result.columns == ["Event", "Duration"])
+    assert all(result["Duration"].values == [4.0, 3.0])
+
+
+@pytest.mark.asyncio
+async def test_one_event_duration_strategy_calculate_duration(
+        one_event_duration_strategy, sample_events
+):
+    result = await one_event_duration_strategy.calculate_duration(
+        sample_events, event_name="Event 2"
+    )
+    assert isinstance(result, pd.DataFrame)
+    assert len(result) == 1
+    assert all(result.columns == ["Date", "Duration"])
+    assert all(result["Duration"].values == [3.0])
+
+
+# @pytest.mark.asyncio
+# async def test_event_duration_periods_strategy_calculate_duration(
+#         event_duration_periods_strategy, sample_events
+# ):
+#     result = await event_duration_periods_strategy.calculate_duration(
+#         sample_events, event_name="Event 1", period_days=1, num_periods=3
+#     )
+#     assert isinstance(result, pd.DataFrame)
+#     assert len(result) == 3
+#     assert all(result.columns == ["Date", "Day", "Duration", "Period"])
+#     assert all(result["Duration"].values == [2.0, 0.0, 0.0])
+
+
+@pytest.mark.asyncio
+async def test_event_duration_periods_strategy_not_enough_data_error(
+    event_duration_periods_strategy, sample_events
+):
+    with pytest.raises(ValueError):
+        await event_duration_periods_strategy.calculate_duration(
+            sample_events, event_name="Event 1", period_days=6, num_periods=2
         )
-        output = self.transformer.many_events_duration(
-            events, max_events=3, ascending=False
-        )
 
-        pd.testing.assert_frame_equal(output, expected_output)
 
-    async def test_one_event_duration(self):
-        events = [
-            {
-                "start": {"dateTime": "2022-02-18T09:00:00+00:00"},
-                "end": {"dateTime": "2022-02-18T10:30:00+00:00"},
-                "summary": "Meeting 1",
-            },
-            {
-                "start": {"dateTime": "2022-02-18T11:00:00+00:00"},
-                "end": {"dateTime": "2022-02-18T12:30:00+00:00"},
-                "summary": "Meeting 1",
-            },
-            {
-                "start": {"dateTime": "2022-02-18T13:00:00+00:00"},
-                "end": {"dateTime": "2022-02-18T14:30:00+00:00"},
-                "summary": "Meeting 2",
-            },
-        ]
-        expected_output = pd.DataFrame({"Date": ["02.18"], "Duration": [3.0]})
-        output = self.transformer.one_event_duration(events, "Meeting 1")
-        pd.testing.assert_frame_equal(output, expected_output)
+@pytest.mark.asyncio
+async def test_async_data_transformer_no_strategy(async_data_transformer, sample_events):
+    with pytest.raises(ValueError):
+        await async_data_transformer.calculate_duration(sample_events)
